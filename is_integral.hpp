3476897b1d8cbe53d24ae5b752a89c5aee1a0104
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-05-24 11:58:04 +0200

    fix is_integral for volatile and cost

--- is_integral.hpp
+++ is_integral.hpp
@@ -5,24 +5,58 @@ namespace ft {
 
 template <class T>
 struct is_integral{
-	static const int value = false;
+	static const bool value = false;
 };
+
+/*
+template <class T>
+struct nq {
+	typedef T type;
+};
+
+template <class T>
+struct nq<const T> {
+	typedef T type;
+};
+
+template <class T>
+struct nq<volatile const T> {
+	typedef T type;
+};
+
+template <class T>
+struct nq<volatile T> {
+	typedef T type;
+};
+*/
+
+#define all_int \
+	is_int(bool);\
+	is_int(char);\
+	is_int(signed char);\
+	is_int(unsigned char);\
+	is_int(wchar_t);\
+	is_int(short);\
+	is_int(unsigned short);\
+	is_int(int);\
+	is_int(unsigned int);\
+	is_int(long);\
+	is_int(unsigned long);\
+	is_int(long long);\
 
 //the compiler tells me:
 //template specialization requires 'template<>'
-#define is_int(type) template <> struct is_integral<type>{ static const int value = true; };
-	is_int(bool);
-	is_int(char);
-	is_int(signed char);
-	is_int(unsigned char);
-	is_int(wchar_t);
-	is_int(short);
-	is_int(unsigned short);
-	is_int(int);
-	is_int(unsigned int);
-	is_int(long);
-	is_int(unsigned long);
-	is_int(long long);
+#define is_int(_type_) template <> struct is_integral<_type_>{ static const bool value = true; };
+	all_int
+#undef is_int
+#define is_int(_type_) template <> struct is_integral<const _type_>{ static const bool value = true; };
+	all_int
+#undef is_int
+#define is_int(_type_) template <> struct is_integral<volatile const _type_>{ static const bool value = true; };
+	all_int
+#undef is_int
+#define is_int(_type_) template <> struct is_integral<volatile _type_>{ static const bool value = true; };
+	all_int
 #undef is_int
 
 
