8d76d0314a3d055eae843d5d830ee228a1a612b8
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-05-30 11:28:09 +0200

    split off node_iterator and pair

---
+++ node_iterator.hpp
@@ -0,0 +1,131 @@
+#ifndef NODE_ITERATOR_HPP
+#define NODE_ITERATOR_HPP
+
+#include "iterator.hpp"
+
+namespace ft {
+
+struct bidirectional_iterator_tag {};
+template <class T, class Compare, class Alloc>
+struct Avlnode;
+
+template <typename T, class Compare, class Alloc, class Pointer = T*, class Reference = T& >
+class node_iterator : public general_iterator<bidirectional_iterator_tag, Avlnode<T, Compare, Alloc> > {
+	typedef general_iterator<bidirectional_iterator_tag, Avlnode<T, Compare, Alloc> > base_type;
+	private:
+	typename base_type::pointer _pointer;
+	public:
+	//Constructors 
+	node_iterator(typename base_type::pointer p) : _pointer(p) {}
+	node_iterator() : _pointer(NULL) {}
+	//TODO copy constructor
+	
+	//Copy assignment
+	node_iterator &operator=(const node_iterator &it) {
+		_pointer = it._pointer;
+		return *this;
+	}
+	//Destructor 
+	~node_iterator() {}
+
+	//Comparison
+	bool operator == (const node_iterator &nodei) const { return _pointer == nodei._pointer; }
+	bool operator != (const node_iterator &nodei) const { return _pointer != nodei._pointer; }
+
+	//Dereference
+	//https://stackoverflow.com/questions/21569483/c-overloading-dereference-operators
+	T* operator->() const { return &(_pointer->_elem); }
+	T& operator*() const { return _pointer->_elem; }
+
+	//increment
+	node_iterator &operator++() {
+		_pointer = _pointer->next();
+		return *this;
+	}
+	node_iterator operator++(int) {
+		node_iterator tmp = *this;
+		_pointer = _pointer->next();
+		return tmp;
+	}
+
+	//decrement
+	node_iterator &operator--() {
+		_pointer = _pointer->previous();
+		return *this;
+	}
+	node_iterator operator--(int) {
+		node_iterator tmp = *this;
+		_pointer = _pointer->previous();
+		return tmp;
+	}
+
+	/*implicit conversion*/
+	operator node_iterator< T, Compare, Alloc, const T*, const T&> () const {
+		return (node_iterator< T, Compare, Alloc, const T*, const T&>(_pointer));
+	}
+};
+
+//https://www.cplusplus.com/reference/iterator/reverse_iterator/
+template <typename T, class Compare, class Alloc, class Pointer = T*, class Reference = T& >
+class rev_node_iterator : public general_iterator<bidirectional_iterator_tag, Avlnode<T, Compare, Alloc> > {
+	typedef general_iterator<bidirectional_iterator_tag, Avlnode<T, Compare, Alloc> > base_type;
+	private:
+	node_iterator<T, Compare, Alloc> _base;
+	public:
+	//constructors
+	rev_node_iterator(typename base_type::pointer p) : _base(p) { }
+	rev_node_iterator() : _base(NULL) { }
+	rev_node_iterator(node_iterator<T, Compare, Alloc> b) : _base(b) { }
+	// copy constructor
+	template <class E>
+	rev_node_iterator(const rev_node_iterator<E, Compare, Alloc> &it) : _base(it.base()) {}
+	//copy assignment
+	rev_node_iterator &operator=(const rev_node_iterator &it) {
+		_base = it._base;
+		return *this;
+	}
+	//Base!
+	rev_node_iterator base() const {return _base;}
+
+	//Destuctor
+	~rev_node_iterator() {};
+
+	//dereference
+	//https://stackoverflow.com/questions/21569483/c-overloading-dereference-operators
+	T* operator->() const { return &(*_base); }
+	T& operator*() const { return *_base; }
+
+	bool operator == (const rev_node_iterator &nodei) const { return _base == nodei._base; }
+	bool operator != (const rev_node_iterator &nodei) const { return _base != nodei._base; }
+
+	//increment
+	rev_node_iterator &operator++() {
+		_base--;
+		return *this;
+	}
+	rev_node_iterator operator++(int) {
+		rev_node_iterator tmp = *this;
+		_base--;
+		return tmp;
+	}	
+
+	//decrement
+	rev_node_iterator &operator--() {
+		_base++;
+		return *this;
+	}
+	rev_node_iterator operator--(int) {
+		rev_node_iterator tmp = *this;
+		_base++;
+		return tmp;
+	}
+
+	/*implicit conversion!*/
+	operator rev_node_iterator< T, Compare, Alloc, const T*, const T&> () const {
+		return (rev_node_iterator< T, Compare, Alloc, const T*, const T&>(_base));
+	}
+};
+
+}
+
+#endif
---
+++ pair.hpp
@@ -0,0 +1,69 @@
+#ifndef PAIR_HPP
+#define PAIR_HPP
+
+namespace ft {
+template <class T1, class T2>
+struct pair{
+	public:
+	//Member types
+	typedef T1 first_type;
+	typedef T2 second_type;
+
+	//Member variables
+	T1 first;
+	T2 second;
+
+	//constructors
+	//default
+	pair() : first(), second() {};
+
+	//copy
+	template<class U, class V>
+		pair (const pair<U,V>& pr) : first(pr.first), second(pr.second) {}
+
+	//initialization
+	pair (const first_type& a, const second_type& b) : first(a), second(b) {}
+
+	//copy assignment
+	pair &operator=(const pair<T1, T2> &p) {
+		first = p.first;
+		second= p.second;
+		return *this;
+	}
+
+	//Destructor
+	~pair() {};
+
+	//implicit conversion
+	operator pair< const T1, const T2> () const
+	{
+		return (pair< const T1, const T2>());
+	}
+};
+
+template <class T1, class T2>
+pair<T1, T2> make_pair(T1 t1, T2 t2) {return pair<T1, T2>(t1, t2);}
+
+template <class T1, class T2>
+std::ostream &operator<<(std::ostream &out, const pair<T1,T2> &p) {
+	out << "(" << p.first << ", " << p.second << ")";
+	return out;
+}
+
+template <class T1, class T2>
+bool operator== (const pair<T1,T2>& a, const pair<T1,T2>& b) {return a.first == b.first && a.second == b.second;}
+template <class T1, class T2>
+bool operator!= (const pair<T1,T2>& a, const pair<T1,T2>& b) {return !(a==b);}
+//This does a lexicographical comparison, or because there's only two, it checks the first, then the second
+template <class T1, class T2>
+bool operator< (const pair<T1,T2>& a, const pair<T1,T2>& b) {return (a.first<b.first) || (a.first==b.first && a.second < b.second);}
+//The rest follows from equality and less than
+template <class T1, class T2>
+bool operator<= (const pair<T1,T2>& a, const pair<T1,T2>& b) { return !(b<a); }
+template <class T1, class T2>
+bool operator>  (const pair<T1,T2>& a, const pair<T1,T2>& b) { return b<a; }
+template <class T1, class T2>
+bool operator>= (const pair<T1,T2>& a, const pair<T1,T2>& b) { return !(a<b); }
+
+}
+#endif
